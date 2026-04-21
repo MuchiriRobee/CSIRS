d3ae93ac5e58cff1345a8ebaa9cea5f2945bcb79
MuchiriRobee <robeemuchiri@gmail.com>
2026-04-21 09:48:04 +0300

    updated schema and register schema on shared types

--- packages/shared/src/schemas/user.schema.ts
+++ packages/shared/src/schemas/user.schema.ts
@@ -1,10 +1,17 @@
-// user.schema.ts
+// src/schemas/user.schema.ts
 import { z } from "zod";
 import { UserRole } from "../types";
 
 export const registerSchema = z.object({
   name: z.string().min(2, "Name must be at least 2 characters"),
   email: z.string().email("Invalid email address"),
+  phone: z
+    .string()
+    .regex(
+      /^\+254[0-9]{9}$/,
+      "Phone number must be in international format: +254 followed by 9 digits (e.g. +254712345678)",
+    )
+    .length(13, "Phone number must be exactly 13 characters"),
   password: z
     .string()
     .min(8, "Password must be at least 8 characters")
