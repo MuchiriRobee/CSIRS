c6429eaecf6227bcd9be93a7401a7eb5c74caf8a
MuchiriRobee <robeemuchiri@gmail.com>
2026-04-22 21:39:30 +0300

    fix build error: could not load packages/shared

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@csirs/shared": path.resolve(__dirname, "../../packages/shared/dist"),
+      "@csirs/shared": path.resolve(__dirname, "../../packages/shared/src"),
     },
   },
 });
