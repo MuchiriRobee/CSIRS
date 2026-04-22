533c96c51039bd39d3823c2ccd36d422e3636d60
MuchiriRobee <robeemuchiri@gmail.com>
2026-04-22 21:24:10 +0300

    vercel json file for reloads

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -8,10 +8,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@csirs/shared": path.resolve(__dirname, "../../packages/shared/src"),
+      "@csirs/shared": path.resolve(__dirname, "../../packages/shared/dist"),
     },
-  },
-  optimizeDeps: {
-    include: ["@csirs/shared"],
   },
 });
